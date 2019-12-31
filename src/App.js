195998032f0db60ffd42b390d5b4d7df617018eb
Melissa Manousos <mmanousos@gmail.com>
2019-12-31 15:34:01 -0500

    Imports airline route data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import DATA from './data.js';
+
 class App extends Component {
   render() {
+    const routeData = DATA.routes.map(route => (
+      <tr>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    ));
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <table>
+            <thead>
+              <th>Airline</th>
+              <th>src</th>
+              <th>dest</th>
+            </thead>
+            <tbody>
+              {routeData}  
+            </tbody>
+          </table>
         </section>
       </div>
     );
@@ -19,3 +36,5 @@ class App extends Component {
 }
 
 export default App;
+
+// for each entry in the routes array, iterate over and create a table row with airline, src & dest
