9d4bc96f41fc50eb3c29b9597f06082d6da9d265
Melissa Manousos <mmanousos@gmail.com>
2020-01-05 17:14:42 -0800

    Adds Select component to filter by airline or airport

--- src/App.js
+++ src/App.js
@@ -4,15 +4,42 @@ import './App.css';
 import DATA from './data.js';
 
 import Table from './components/Table';
+import Select from './components/Select';
 
 class App extends Component {
+  state = {
+    airline: 'all',
+    airport: 'all',
+  };
+
+  resetDefault = () => {
+    this.setState({ airline: 'all', airport: 'all' });
+  };
+
   formatValue = (property, value) => {
     if (property === 'airline') {
       return DATA.getAirlineById(value).name;
     } else {
       return DATA.getAirportByCode(value).name;
     }
-  }
+  };
+
+  airlineSelected = (value) => {
+    if (value !== 'all') {
+      value = parseInt(value, 10);
+    }
+    this.setState({ airline: value });
+  };
+
+  airportSelected = (value) => { this.setState({ airport: value }) };
+
+  routeHasCurrentAirline = (route) => {
+    return route.airline === this.state.airline || this.state.airline === 'all';
+  };
+
+  routeHasCurrentAirport = (route) => {
+    return route.src === this.state.airport || route.dest === this.state.airport || this.state.airport === 'all';
+  };
 
   render() {
     const columns = [
@@ -21,15 +48,59 @@ class App extends Component {
       { name: 'Destination Airport', property: 'dest' },
     ];
 
-    const routes = DATA.routes;
+    const filteredRoutes = DATA.routes.filter( (route) => {
+      return this.routeHasCurrentAirline(route) && this.routeHasCurrentAirport(route);
+    });
+
+    const filteredRoutesByAirline = DATA.routes.filter(this.routeHasCurrentAirline);
+
+    const filteredRoutesByAirport = DATA.routes.filter(this.routeHasCurrentAirport);
+
+    const filteredAirlines = DATA.airlines.filter( (airline) => {
+      return filteredRoutesByAirline.some( (route) => route.airline === airline.id );
+    });
+
+    const filteredAirports = DATA.airports.filter( (airport) => {
+      return filteredRoutesByAirport.some( (route) => route.src === airport.code || route.dest === airport.code );
+    });
 
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
+
         <section>
-          <Table className="routes-table" columns={columns} rows={routes} format={this.formatValue} perPage="25" />
+          <p>
+            Show routes on 
+            <Select 
+              options={filteredAirlines} 
+              valueKey="id" 
+              titleKey="name"
+              enabledKey="active"
+              allTitle="All Airlines"
+              value={this.state.airline}
+              onSelect={this.airlineSelected}
+            />
+            flying in or out of 
+            <Select
+              options={filteredAirports}
+              valueKey="code"
+              titleKey="name"
+              enabledKey="active"
+              allTitle="All Airports"
+              value={this.state.airport}
+              onSelect={this.airportSelected}
+            />
+          </p>
+          
+          <Table 
+            className="routes-table" 
+            columns={columns} 
+            rows={filteredRoutes} 
+            format={this.formatValue} 
+            perPage="25" 
+          />
         </section>
       </div>
     );
---
+++ src/components/Select.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Select extends Component {
+  static defaultProps = {
+    options: [],
+    title: 'Please select',
+    valueKey: '',
+    titleKey: '',
+    value: 'all',
+    allTitle: 'All',
+    onSelect: (value) => null,
+    enabledKey: undefined,
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.props.onSelect(event.target.value);
+  }
+
+  render() {
+    let options = this.props.options.map( (option) => {
+      const value = option[this.props.valueKey];
+      return <option key={value} value={value}>
+        { option[this.props.titleKey] }
+      </option>;
+    });
+    options.unshift(<option key="all" value="all">{this.props.allTitle}</option>);
+
+    return (
+      <select value={this.props.value} onChange={this.handleChange} >
+        { options }
+      </select>
+    );
+  }
+}
+
+export default Select;
